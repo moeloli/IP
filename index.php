c261f0c375627f96e90ee3706cbce6197d9da942
abc1763613206 <abc1763613206@163.com>
2025-07-02 15:29:09 +0800

    fix: ip api
    我在 GitHub 修文物

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $get=$_GET["s"];
 $get=base64_decode(str_replace(" ","+",$get));
 //$wangzhi=$_SERVER['HTTP_REFERER'];这里获取当前网址
 //here is ip 
-$url="http://ip.taobao.com/service/getIpInfo.php?ip=".$ip; 
+$url="https://api.live.bilibili.com/ip_service/v1/ip_service/get_ip_addr?ip=".$ip; 
 $UserAgent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 3.0.04506; .NET CLR 3.5.21022; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';  
 $curl = curl_init(); 
 curl_setopt($curl, CURLOPT_URL, $url); 
@@ -31,7 +31,7 @@ curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
 $data = curl_exec($curl);
 $data = json_decode($data, true);
 $country = $data['data']['country']; 
-$region = $data['data']['region']; 
+$region = $data['data']['province']; 
 $city = $data['data']['city'];
 //定义颜色
 $black = ImageColorAllocate($im, 0,0,0);//定义黑色的值
